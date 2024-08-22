839b2193cd54726aec485bff08ddd62b1822617f
maytech76 <yanezmarco0576@gmail.com>
2024-08-21 20:54:20 -0500

    21-08-2024 Modifiq Table models to model_cars, ejecute migrate and seeder)

--- app/Models/Modelcar.php
+++ app/Models/Modelcar.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Model extends Model
+class Modelcar extends Model
 {
     use HasFactory;
+
+    protected $table = 'model_cars';
+
+    protected $fillable = [
+
+        'name', 'user_id'
+    ];
 }
--- database/seeders/ModelCarSeeder.php
+++ database/seeders/ModelCarSeeder.php
@@ -2,24 +2,35 @@
 
 namespace Database\Seeders;
 
+use App\Models\Modelcar;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+
 
 class ModelCarSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+
+        public function run(): void
     {
         {
-            $models = [
+            $modelos = [
              'Poer',
              'Vigus',
              'Grubber',
              'Vision',
              'Sport'
             ];
+
+            foreach ( $modelos as  $modelo) {
+                Modelcar::create([
+                    'name' =>  $modelo,
+                ]);
+            }
         }
     }
-}
+    }
+
